ce66abd79232f2de07f928551ad3af9ae7830dce
Tyler Fisher <tyler@tylerjfisher.com>
2013-07-27 17:45:26 -0400

    we have crayons!

---
+++ js/main.js
@@ -0,0 +1,25 @@
+parseData = function(data) {
+
+    var families = _.chain(data)
+        .groupBy(function(d){ return Number(d.family); })
+        .value();
+        console.log(families);
+
+    _.each(families, makeCircle);
+
+}
+
+makeCircle = function(family) {
+
+    $('#families').append('<div class="family"></div>');
+
+    _.each(family, function(family, i) {
+        $('.family').last().append('<div class="crayon"></div>');
+        $('.crayon').last().addClass('pos-' + i);
+        $('.crayon').last().css('background', 'rgb(' + family.rgb + ')');
+        $('.crayon').last().css('border-bottom-color', 'rgb(' + family.rgb + ')')
+    });
+
+}
+
+parseData(crayons);
