7c26882c51960fd4f25de19f081ee0c9bef5027c
danhillreports <danhillreports@gmail.com>
2013-07-28 22:02:57 -0400

    initial

---
+++ js/main.js
@@ -0,0 +1,34 @@
+parseData = function(data) {
+
+    var families = _.chain(data)
+        .groupBy(function(d){ return Number(d.family); })
+        .value();
+        console.log(families);
+
+    _.each(families, makeCircle);
+
+}
+
+makeCircle = function(family) {
+
+    $('#families').append('<div class="family"></div>');
+
+    _.each(family, function(family, i) {
+        $('.family').last().append('<div class="crayon"><div class="crayon-tip"></div></div>');
+        $('.crayon').last().addClass('pos-' + i);
+        $('.crayon').last().css('background', 'rgb(' + family.rgb + ')');
+        $('.crayon').last().css('border-bottom-color', 'rgb(' + family.rgb + ')')
+        $('.crayon-tip').last().css('border-bottom-color', 'rgb(' + family.rgb + ')');
+
+        $('.crayon').last().on('click', function() {
+            var palette_item = document.createElement('div');
+            palette_item.className = 'palette-item';
+            var rgb = this.style.background;
+            palette_item.style.backgroundColor=rgb;
+            palette_item.innerHTML = rgb;
+            $('#palette-container').append(palette_item);
+        })
+    });
+}
+
+parseData(crayons);
